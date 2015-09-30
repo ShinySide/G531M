1775d4eae070c4ac76eb1a0c73548e004a1816a1
ShinySide <shinyside0524@gmail.com>
2015-09-30 18:11:53 -0400

    stock

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Sep 30 18:02:14 EDT 2015"
+#define LINUX_COMPILE_BY "adam0524"
+#define LINUX_COMPILE_HOST "bigAK"
+#define LINUX_COMPILER "gcc version 4.9.4 20150920 (prerelease) (UBERTC-4.9.4) "
